18b3ff7b5d5dc714d618219d1f518a4b3176ac0e
kitaq_itclub <resuscitation.project@gmail.com>
2026-07-20 00:21:43 +0900

    Fix image path formatting in config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ window.BEPPO_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbxyD3yThxdK6Irc_jV88mnbZ3bqKAcURPNwvZLfl5f6XyyIltjiwN-25nLznNQ3VWOQ1A/exec",
   demoMode: false,
   requestTimeoutMs: 30000,
-  images: { hero: "./assets/beppo_shop01.jpeg", today: "./assets/beppo_curry01.jpeg", shop: "./assets/beppo-03.png", pottery: "./assets/beppo-04.png", dessert: "./assets/curry-placeholder.svg" }
+  images: { hero: "./assets/beppo_shop01.jpeg", today: "./assets/beppo_curry01.jpeg", shop: "./assets/beppo_03.png", pottery: "./assets/beppo_04.png", dessert: "./assets/curry-placeholder.svg" }
 };
